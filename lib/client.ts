802e2c5a6c584b75f3f43e34fc565ed2485c5dc2
Micah <ismiki977@gmail.com>
2023-11-20 19:21:30 +0300

    Bug fixes

--- lib/client.ts
+++ lib/client.ts
@@ -1,6 +1,12 @@
 import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId, useCdn, authToken } from "../sanity/env";
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-11-18'
+
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const authToken = process.env.NEXT_PUBLIC_SANITY_AUTH_TOKEN;
+export const useCdn = false  
 
 export const client = createClient({
   apiVersion,
--- lib/image.ts
+++ lib/image.ts
@@ -1,7 +1,10 @@
 import createImageUrlBuilder from '@sanity/image-url'
 import { SanityImageSource } from '@sanity/image-url/lib/types/types'
 
-import { dataset, projectId } from '../sanity/env'
+
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+ 
 
 const imageBuilder = createImageUrlBuilder({
   projectId: projectId || '',
